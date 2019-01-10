42fb2f29dea78299827983cfbdbe13de8997a203
CoderDream <coderdream@gmail.com>
2019-01-10 16:29:31 +0800

    重构

--- Chapter03/activiti6-helloworld/src/main/java/com/coderdream/activiti/helloworld/DemoMain.java
+++ Chapter03/activiti6-helloworld/src/main/java/com/coderdream/activiti/helloworld/DemoMain.java
@@ -1,6 +1,11 @@
 package com.coderdream.activiti.helloworld;
 
+import com.google.common.collect.Maps;
 import org.activiti.engine.*;
+import org.activiti.engine.form.FormProperty;
+import org.activiti.engine.form.TaskFormData;
+import org.activiti.engine.impl.form.DateFormType;
+import org.activiti.engine.impl.form.StringFormType;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.engine.repository.ProcessDefinition;
@@ -9,7 +14,12 @@ import org.activiti.engine.task.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class DemoMain {
 
@@ -18,15 +28,79 @@ public class DemoMain {
      */
     private static final Logger LOGGER = LoggerFactory.getLogger(DemoMain.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         LOGGER.info("启动我们的程序");
         // 创建流程引擎
-        ProcessEngineConfiguration cfg = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration();
-        ProcessEngine processEngine = cfg.buildProcessEngine();
-        String name = processEngine.getName();
-        String version = ProcessEngine.VERSION;
-        LOGGER.info("流程引擎名称{}，版本{}", name, version);
+        ProcessEngine processEngine = getProcessEngine();
+
         // 部署流程定义文件
+        ProcessDefinition processDefinition = getProcessDefinition(processEngine);
+
+        // 启动运行流程
+        ProcessInstance processInstance = getProcessInstance(processEngine, processDefinition);
+
+        // 处理流程任务
+        processTask(processEngine, processInstance);
+
+        LOGGER.info("结束我们的程序");
+    }
+
+    private static void processTask(ProcessEngine processEngine, ProcessInstance processInstance) throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        while (processInstance != null && !processInstance.isEnded()) {
+            TaskService taskService = processEngine.getTaskService();
+            List<Task> list = taskService.createTaskQuery().list();
+            LOGGER.info("待处理任务数量 [{}]", list.size());
+            for (Task task : list) {
+                LOGGER.info("待处理任务 [{}]", task.getName());
+                Map<String, Object> variables = getMap(processEngine, scanner, task);
+
+                // 提交工作
+                taskService.complete(task.getId(), variables);
+                // 同步流程状态
+                processInstance = processEngine
+                        .getRuntimeService()
+                        .createProcessInstanceQuery()
+                        .processInstanceId(processInstance.getId())
+                        .singleResult();
+            }
+        }
+        scanner.close();
+    }
+
+    private static Map<String, Object> getMap(ProcessEngine processEngine, Scanner scanner, Task task) throws ParseException {
+        FormService formService = processEngine.getFormService();
+        TaskFormData taskFormData = formService.getTaskFormData(task.getId());
+        List<FormProperty> formProperties = taskFormData.getFormProperties();
+        Map<String, Object> variables = Maps.newHashMap();
+        for (FormProperty property : formProperties) {
+            String line = null;
+            if (StringFormType.class.isInstance(property.getType())) {
+                LOGGER.info("请输入 {} ?", property.getName());
+                line = scanner.nextLine();
+                variables.put(property.getId(), line);
+            } else if (DateFormType.class.isInstance(property.getType())) {
+                LOGGER.info("请输入 {} ? 格式 (yyyy-MM-dd)", property.getName());
+                line = scanner.nextLine();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = dateFormat.parse(line);
+                variables.put(property.getId(), date);
+            } else {
+                LOGGER.info("类型暂不支持  [{}] ?", property.getType());
+            }
+            LOGGER.info("您输入的内容是  [{}] ?", line);
+        }
+        return variables;
+    }
+
+    private static ProcessInstance getProcessInstance(ProcessEngine processEngine, ProcessDefinition processDefinition) {
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
+        LOGGER.info("启动流程 [{}]", processInstance.getProcessDefinitionKey());
+        return processInstance;
+    }
+
+    private static ProcessDefinition getProcessDefinition(ProcessEngine processEngine) {
         RepositoryService repositoryService = processEngine.getRepositoryService();
         DeploymentBuilder deploymentBuilder = repositoryService.createDeployment();
         deploymentBuilder.addClasspathResource("second_approve.bpmn");
@@ -37,17 +111,15 @@ public class DemoMain {
                 .deploymentId(deploymentId)
                 .singleResult();
         LOGGER.info("流程定义文件 [{}]，流程ID [{}]", processDefinition.getName(), processDefinition.getId());
-        // 启动运行流程
-        RuntimeService runtimeService = processEngine.getRuntimeService();
-        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
-        LOGGER.info("启动流程 [{}]", processInstance.getProcessDefinitionKey());
-        // 处理流程任务
-        TaskService taskService = processEngine.getTaskService();
-        List<Task> list = taskService.createTaskQuery().list();
-        for (Task task : list) {
-            LOGGER.info("待处理任务 [{}]", task.getName());
-        }
-        LOGGER.info("待处理任务数量 [{}]", list.size());
-        LOGGER.info("结束我们的程序");
+        return processDefinition;
+    }
+
+    private static ProcessEngine getProcessEngine() {
+        ProcessEngineConfiguration cfg = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration();
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+        String name = processEngine.getName();
+        String version = ProcessEngine.VERSION;
+        LOGGER.info("流程引擎名称{}，版本{}", name, version);
+        return processEngine;
     }
 }
