ff77b98214f751509b0f3f997ca0f914a6d6c234
CoderDream <coderdream@gmail.com>
2019-01-14 21:49:07 +0800

    update

---
+++ Chapter04/activiti6-samples/config3/src/main/java/com/coderdream/activiti/App.java
@@ -0,0 +1,13 @@
+package com.coderdream.activiti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
