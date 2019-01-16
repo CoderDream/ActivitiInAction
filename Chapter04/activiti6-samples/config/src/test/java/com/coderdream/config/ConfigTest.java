4191f16883e2a79ad1c395408c795f0518758c29
CoderDream <coderdream@gmail.com>
2019-01-16 15:47:59 +0800

    4-2 创建流程引擎配置-config_samples

---
+++ Chapter04/activiti6-samples/config/src/test/java/com/coderdream/config/ConfigTest.java
@@ -0,0 +1,35 @@
+package com.coderdream.config;
+
+
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConfigTest {
+
+    /**
+     * logger
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigTest.class);
+
+    @Test
+    public void testConfig1() {
+        ProcessEngineConfiguration configuration = ProcessEngineConfiguration
+                .createProcessEngineConfigurationFromResourceDefault();
+
+        LOGGER.info("configuration = {}", configuration);
+    }
+
+    @Test
+    public void testConfig2() {
+        ProcessEngineConfiguration configuration = ProcessEngineConfiguration
+                .createStandaloneProcessEngineConfiguration();
+
+
+        SpringProcessEngineConfiguration a = null;
+        LOGGER.info("configuration = {}", configuration);
+    }
+
+}
